0c353229648294e6311f52301451ee07528fd0bf
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-12-07 22:49:31 +0200

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
-"""commenttttttttttttttttttttttttttttttttttt"""
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
-import datetime
-import sys
-import json
+from datetime import datetime
+
+# from models import storage
 
 
 class BaseModel:
-    """commenttttttttttttttttttttttttttttttttttt"""
-    id = None
-    created_at = None
-    updated_at = None
+    """A base class for all hbnb models"""
 
     def __init__(self, *args, **kwargs):
-        """commenttttttttttttttttttttttttttttttttttt"""
-        pass
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # storage.new(self)
+        else:
+            self.__dict__["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            self.__dict__["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            del kwargs["__class__"]
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """commenttttttttttttttttttttttttttttttttttt"""
-        pass
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """commenttttttttttttttttttttttttttttttttttt"""
-        pass
+        """Updates updated_at with current time when instance is changed"""
+
+        self.updated_at = datetime.now()
+        # storage.save()
 
     def to_dict(self):
-        """commenttttttttttttttttttttttttttttttttttt"""
-        pass
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
