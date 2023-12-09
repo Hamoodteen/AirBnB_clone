1a89df5a5d1f56f599f287b6b39b0fe8d6cd1aaf
Hamoodteen <ahmed104567@outlook.com>
2023-12-09 22:53:39 +0200

    0

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,6 @@
 
 import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -17,6 +10,13 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
+    from models.base_model import BaseModel
+    from models.user import User
+    from models.place import Place
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.review import Review
     classes = {
         "BaseModel": BaseModel,
         "User": User,
