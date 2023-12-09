aa8a45472e5f2b1e434c47228f9042a061313c02
Hamoodteen <ahmed104567@outlook.com>
2023-12-09 23:13:54 +0200

    0

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,13 @@
 
 import json
 import os
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -28,13 +34,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file to recreate objects"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
             "BaseModel": BaseModel,
