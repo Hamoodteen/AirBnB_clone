c07da7bc0c72518cf9e77bdfcac7c224dffe370b
Hamoodteen <ahmed104567@outlook.com>
2023-12-09 22:55:11 +0200

    0

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,13 @@ a dictionary(class FileStorage 's attribute).
 so all objects are stored in a dictionary in class FileStorage
 and can be accessed by instance of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 storage = FileStorage()
