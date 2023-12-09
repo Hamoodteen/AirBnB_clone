303ca511313d43bd54835ba0c3c37eed8234e27e
Hamoodteen <ahmed104567@outlook.com>
2023-12-09 23:35:55 +0200

    0

--- console.py
+++ console.py
@@ -74,7 +74,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, line):
         """commenttttttttttttttttttttttttttttttttttt"""
-        pass
+        tok = line.split()
+        if tok[0] is not None and tok[0] not in clss:
+            print("** class doesn't exist **")
+            return
+        print(storage.all())
 
     def do_update(self):
         """commenttttttttttttttttttttttttttttttttttt"""
