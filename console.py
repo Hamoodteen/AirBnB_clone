77655f47dc25fdf8f08a89a9c1e8fac8dc12fabd
Hamoodteen <ahmed104567@outlook.com>
2023-12-09 23:08:28 +0200

    0

--- console.py
+++ console.py
@@ -4,7 +4,6 @@ import cmd
 import json
 from models.base_model import BaseModel
 from models import storage
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
