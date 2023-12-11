9e71c0b32763fe3d283c3f046d0bf1002caf3adc
Hamoodteen <132084164+Hamoodteen@users.noreply.github.com>
2023-12-11 12:58:14 +0200

    0

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,7 @@ class BaseModel:
 
         self.updated_at = datetime.now()
         storage.save()
+        print("OK")
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
@@ -49,4 +50,5 @@ class BaseModel:
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        print("OK")
         return my_dict
